c065122c0b378270589cd733e341523f586425ef
Rogerio Rodrigues <rogerio.s.rodrigues@avanade.com>
2019-02-07 15:40:51 -0200

    .
    .

---
+++ spring-ajax/src/main/java/com/aplication/spring/DemoAjaxApplication.java
@@ -0,0 +1,14 @@
+package com.aplication.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAjaxApplication.class, args);
+	}
+
+}
+
---
+++ spring-ajax/src/main/java/service/SocialMetaTagService.java
@@ -0,0 +1,17 @@
+package service;
+
+import org.springframework.stereotype.Service;
+
+import domain.SocialMetaTag;
+
+@Service
+public class SocialMetaTagService {
+
+	
+	//methodo 
+	public SocialMetaTag getOpenGraphbyUrl(String url) {
+		SocialMetaTag 
+		
+	}
+	
+}
